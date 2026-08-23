23ae177a326e0a69b74b869c74f8830592686cfd
xuanmingliu <xuanmingliu0120@gmail.com>
2026-08-23 12:23:16 +0800

    Delete datasets directory

--- datasets/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Datasets package for XYZFlow distillation utilities."""
-
-from .trajectory_sequence import TrajectorySequenceDataset, build_dataloader  # noqa: F401
-from .xyzflow_sequence_dataset import XYZFlowSequenceDataset  # noqa: F401
